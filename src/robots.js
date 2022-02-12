d65feb62eefc15db4448da1eef383c46f135d125
Icevalley <a.icevalley@gmail.com>
2022-02-12 11:22:31 +0100

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Robert Bertsen',
+        username: 'Bert',
+        email: 'Ro.be@gmil.ve'
+    },
+    {
+        id: 2,
+        name: 'Android Beep',
+        username: 'Andy',
+        email: 'Android@online.no'
+    },
+    {
+        id: 3,
+        name: 'Alexa',
+        username: 'Alexa',
+        email: 'Alexa@samsung.com'
+    },
+    {
+        id: 4,
+        name: 'Siri',
+        username: 'Siri',
+        email: 'Siri@apple.com'
+    },
+    {
+        id: 5,
+        name: 'Robert Bertsen',
+        username: 'Bert',
+        email: 'Ro.be@gmil.ve'
+    },
+    {
+        id: 6,
+        name: 'Robert Bertsen',
+        username: 'Bert',
+        email: 'Ro.be@gmil.ve'
+    },
+    {
+        id: 7,
+        name: 'Robert Bertsen',
+        username: 'Bert',
+        email: 'Ro.be@gmil.ve'
+    }
+]
